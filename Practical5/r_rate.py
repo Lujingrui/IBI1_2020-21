6e9435a0f03e08be7ea49a39bc75c7f6b9c24a57
Lujingrui <1208006909@qq.com>
2021-03-10 11:48:18 +0800

    Practical2

---
+++ Practical5/r_rate.py
@@ -0,0 +1,8 @@
+n=84
+r=1.1
+m=0
+while m<5:
+ a=n+n*r
+ n=a
+ m=m+1
+print a
---
+++ Practical5/variables.py
@@ -0,0 +1,14 @@
+a=11129
+b=190784
+c=210310
+d=c-a
+e=b-a
+d<a
+X=1
+Y=0
+Z=2
+Z=(X and not Y) or (Y and not X)
+print Z
+W=2
+W=X!=Y
+Z=W
