0701115f2120fc9e5ee13645bb142acd6c777a62
Lujingrui <1208006909@qq.com>
2021-03-17 16:16:42 +0800

    practical6

--- Practical6/dictionary.py
+++ Practical6/dictionary.py
@@ -2,8 +2,8 @@ genes = {'USA':29862124, 'India':11285561, 'Brazil':11205972, 'Russia':4360823, 
 import matplotlib.pyplot as plt
 lables = 'USA', 'India', 'Brazil', 'Russia', 'UK'
 sizes = [29862124, 11285561, 11205972, 4360823, 4234924]
-explode = (0.1, 0, 0, 0, 0)
-plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+explode = (0, 0, 0, 0, 0)
+plt.pie(sizes, explode=explode, lables=lables, autopct='%1.1f%%',
         shadow=False, startangle=90)
 plt.axis('equal') 
 plt.show()
