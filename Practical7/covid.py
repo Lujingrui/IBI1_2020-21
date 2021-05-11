a54efefe07617406be071a8751c7c50954dd38bc
Lujingrui <1208006909@qq.com>
2021-05-11 16:34:13 +0800

    practical7

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -19,12 +19,15 @@ plt.boxplot(world_new_cases,
             showfliers = True,
             notch = False
               )
+plt.title('new cases worldwide')
 plt.show()
 world_date = covid_data.loc[covid_data['location']=='World','date']
 world_new_deaths = covid_data.loc[covid_data['location']=='World','new_deaths']
 plt.plot(world_date, world_new_cases, 'b', world_date, world_new_deaths, 'r')
+plt.title('new cases and new deaths worldwide')
 plt.show()
 Austria_date = covid_data.loc[covid_data['location']=='Austria','date']
 Austria_new_cases = covid_data.loc[covid_data['location']=='Austria','new_cases']
 plt.plot(Austria_date, Austria_new_cases,'b')
+plt.title('new cases developed over time in Australia')
 plt.show()
