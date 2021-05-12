95ac25f3329655c35e731d646cad51cc42499fd7
Lujingrui <1208006909@qq.com>
2021-05-12 20:04:54 +0800

    practical10

--- Practical10/grade_calculator_function.py
+++ Practical10/grade_calculator_function.py
@@ -1,5 +1,5 @@
 def grade(name,code_portfolio,poster_presentation,final_exam):
-grade = code_portfolio * 0.4 + poster_presentation * 0.3 + final_exam * 0.3
-print(name + ' ' +str(grade))
-
+  grade = code_portfolio * 0.4 + poster_presentation * 0.3 + final_exam * 0.3
+  print(name + ' ' +str(grade))
+grade("Lujingrui",80,70,80)
 
--- Practical10/reverse_complement_calculator.py
+++ Practical10/reverse_complement_calculator.py
@@ -1,10 +1,10 @@
+seq = input("seq: ")
 def f(seq):
     translation = {'A':'T','T':'A','G':'C','C':'G'}
     new_seq = ''
     for n in range(0,len(seq),1):
         new_seq = translation[seq[n]] + new_seq
-    print(new_seq)
-seq = 'ATGCTGC'
+    print("reverse complement: " + new_seq)
 f(seq)
   
 
